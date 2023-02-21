d390f3033c351e49bb044d3b71d6729418017dec
islemfer <islem.ferchichi@esprit.tn>
2023-02-21 11:53:39 +0100

    islem commit forum entities

--- src/main/java/esprit/tn/projetspringbootangular/Entities/Post.java
+++ src/main/java/esprit/tn/projetspringbootangular/Entities/Post.java
@@ -1,14 +1,19 @@
 package esprit.tn.projetspringbootangular.Entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+
 @Entity
 public class Post {
     @Id
